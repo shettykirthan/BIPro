785bec97f7cefd5d0e78a8180fd2fab0954f0b38
shettykirthan <nnm22ad051@nmamit.in>
2024-10-02 01:40:47 +0530

    backend

---
+++ api/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js"
+import authRoute from "./routes/auth.route.js"
+import cookieParser from 'cookie-parser'
+import cors from "cors"
+dotenv.config()
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+app.use(cors())
+
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("MOngoDB connected")
+})
+.catch(()=>{
+    console.log("Unable to coonnect MOngoDb");
+    
+})
+
+
+app.use("/api/user",userRoute)
+app.use("/api/auth",authRoute)
+
+app.listen(9001,()=>{
+    console.log("Server is running");
+    
+})
